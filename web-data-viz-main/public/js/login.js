7f792232188e83538619060a354503fbede90052
Robert1730 <robert.souza@sptech.school>
2024-11-20 14:35:52 -0300

    Login Finalizado

---
+++ web-data-viz-main/public/js/login.js
@@ -0,0 +1,57 @@
+function logar() {
+    var emailVar = document.getElementById("ipt_email").value;
+    var senhaVar = document.getElementById("ipt_senha").value;
+
+    if (emailVar == "") {
+        div_validaçãoEmail.innerHTML = `Insira algo no campo email`
+
+    } else if(senhaVar == "") {
+        div_validaçãoSenha.innerHTML = `Insira algo no campo senha`
+
+    }
+
+    console.log("FORM LOGIN: ", emailVar);
+    console.log("FORM SENHA: ", senhaVar);
+
+    fetch("/usuarios/autenticar", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            emailServer: emailVar,
+            senhaServer: senhaVar
+        })
+    }).then(function (resposta) {
+        console.log("ESTOU NO THEN DO entrar()!")
+
+        if (resposta.ok) {
+            console.log(resposta);
+
+            resposta.json().then(json => {
+                console.log(json);
+                console.log(JSON.stringify(json));
+                sessionStorage.EMAIL_USUARIO = json.email;
+                sessionStorage.NOME_USUARIO = json.nome;
+
+                setTimeout(function () {
+                    window.location = "../index.html";
+                }, 1000); // apenas para exibir o loading
+            });
+
+        } else {
+
+            console.log("Houve um erro ao tentar realizar o login!");
+
+            resposta.text().then(texto => {
+                console.error(texto);
+                finalizarAguardar(texto);
+            });
+        }
+
+    }).catch(function (erro) {
+        console.log(erro);
+    })
+
+    return false;
+}
