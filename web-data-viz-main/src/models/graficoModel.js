53616842d3d04e34b05c1f54be26a18ca7454d02
Robert1730 <robert.souza@sptech.school>
2024-11-21 23:59:36 -0300

    Adicionado o model da dashboard

---
+++ web-data-viz-main/src/models/graficoModel.js
@@ -0,0 +1,37 @@
+var database = require("../database/config")
+
+
+function buscarDadosSistema(id) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function buscarDadosG():" + id);
+    
+    var instrucaoSql = `
+        SELECT COUNT(fkSistema) AS "Quantidade", 
+            sistema.nome AS "Sistema" 
+        FROM usuario 
+            JOIN sistema ON fkSistema = idSistema 
+        GROUP BY sistema.nome;
+    `;
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql)
+}
+
+function buscarDadosTipo(id) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function buscarDadosG():" + id);
+    
+    var instrucaoSql = `
+        SELECT COUNT(fkTipoDeJogador) AS "Quantidade", 
+            tipoDeJogador.nome AS "Tipo" 
+        FROM usuario 
+            JOIN tipoDeJogador ON fktipoDeJogador = idtipoDeJogador 
+        GROUP BY tipoDeJogador.nome;
+    `;
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql)
+}
+
+module.exports = {
+    buscarDadosSistema,
+    buscarDadosTipo
+};
