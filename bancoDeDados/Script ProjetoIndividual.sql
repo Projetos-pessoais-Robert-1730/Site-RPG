4815934f58d671c5d5957d115c7857176a3a105f
Robert1730 <robert.souza@sptech.school>
2024-11-26 12:49:38 -0300

    Atualização BD com view e procedure

--- bancoDeDados/Script ProjetoIndividual.sql
+++ bancoDeDados/Script ProjetoIndividual.sql
@@ -23,64 +23,84 @@ CREATE TABLE usuario (
     FOREIGN KEY (fkSistema) REFERENCES sistema (idSistema)
 );
 
--- CREATE TABLE fichas (
---     idFichas INT PRIMARY KEY AUTO_INCREMENT,
---     nome VARCHAR(50),
---     antecedente VARCHAR(20),
---     tendencia VARCHAR(15),
---     classeNivel VARCHAR(50),
---     raca VARCHAR(50),
---     pontosDeExperiencia INT,
---     fkUsuario INT,
---     FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario)
--- );
+CREATE TABLE fichas (
+    idFichas INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50),
+    antecedente VARCHAR(20),
+    tendencia VARCHAR(15),
+    classeNivel VARCHAR(50),
+    raca VARCHAR(50),
+    pontosDeExperiencia INT,
+    fkUsuario INT,
+    FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario)
+);
 
--- CREATE TABLE atributos (
---     idAtributos INT PRIMARY KEY AUTO_INCREMENT,
---     forca INT,
---     destreza INT,
---     constituicao INT,
---     inteligencia INT,
---     sabedoria INT,
---     carisma INT,
---     proficiencia INT,
---     inspiracao INT,
---     fkFichas INT,
---     FOREIGN KEY (fkFichas) REFERENCES fichas(idFichas)
--- );
+CREATE TABLE atributos (
+    idAtributos INT PRIMARY KEY AUTO_INCREMENT,
+    forca INT,
+    destreza INT,
+    constituicao INT,
+    inteligencia INT,
+    sabedoria INT,
+    carisma INT,
+    proficiencia INT,
+    inspiracao INT,
+    fkFichas INT,
+    FOREIGN KEY (fkFichas) REFERENCES fichas(idFichas)
+);
 
--- CREATE TABLE dinheiro (
---     idDinheiro INT PRIMARY KEY AUTO_INCREMENT,
---     platina INT,
---     ouro INT,
---     elektron INT,
---     prata INT,
---     cobre INT,
---     fkFichas INT,
---     FOREIGN KEY (fkFichas) REFERENCES fichas(idFichas)
--- );
+CREATE TABLE dinheiro (
+    idDinheiro INT PRIMARY KEY AUTO_INCREMENT,
+    platina INT,
+    ouro INT,
+    elektron INT,
+    prata INT,
+    cobre INT,
+    fkFichas INT,
+    FOREIGN KEY (fkFichas) REFERENCES fichas(idFichas)
+);
 
--- CREATE TABLE personalidade (
---     idPersonalidade INT PRIMARY KEY AUTO_INCREMENT,
---     tracosDePersonalidade VARCHAR(150),
---     ideais VARCHAR(150),
---     ligacoes VARCHAR(150),
---     defeitos VARCHAR(150),
---     fkFichas INT,
---     FOREIGN KEY (fkFichas) REFERENCES fichas(idFichas)
--- );
+CREATE TABLE personalidade (
+    idPersonalidade INT PRIMARY KEY AUTO_INCREMENT,
+    tracosDePersonalidade VARCHAR(150),
+    ideais VARCHAR(150),
+    ligacoes VARCHAR(150),
+    defeitos VARCHAR(150),
+    fkFichas INT,
+    FOREIGN KEY (fkFichas) REFERENCES fichas(idFichas)
+);
 
--- CREATE TABLE caracteristicas (
---     idCaracteristicas INT PRIMARY KEY AUTO_INCREMENT,
---     vida INT,
---     vidaTemporaria INT,
---     armadura INT,
---     deslocamento INT,
---     qtdDadosDeVida INT,
---     valorDadosDeVida INT,
---     fkFichas INT,
---     FOREIGN KEY (fkFichas) REFERENCES fichas(idFichas)
--- );
+CREATE TABLE caracteristicas (
+    idCaracteristicas INT PRIMARY KEY AUTO_INCREMENT,
+    vida INT,
+    vidaTemporaria INT,
+    armadura INT,
+    deslocamento INT,
+    qtdDadosDeVida INT,
+    valorDadosDeVida INT,
+    fkFichas INT,
+    FOREIGN KEY (fkFichas) REFERENCES fichas(idFichas)
+);
+
+-- Verificação das tabelas:
+
+SHOW TABLES;
+
+DESC TABLE tipoDeJogador;
+
+DESC TABLE sistema;
+
+DESC TABLE usuario;
+
+DESC TABLE fichas;
+
+DESC TABLE dinheiro;
+
+DESC TABLE personalidade;
+
+DESC TABLE caracteristicas;
+
+-- Primeiros Inserts (Obrigatório para a utilização da API de forma correta, pois serão as FKs da escolha do usuário no frontEnd)
 
 INSERT INTO tipoDeJogador (nome) VALUES 
 ("Jogador"),
@@ -93,17 +113,23 @@ INSERT INTO sistema (nome) VALUES
 ("Tormenta 20"),
 ("Outros");
 
+-- Exemplo de criação de usuário Administrador 
+
 INSERT INTO usuario (nome, email, senha, fkSistema, fkTipoDeJogador) VALUES
 ("Robert Ferreira", "robert.souza@sptech.school", "123456789", "1", "3");
 
--- Selects utilizados em fetchs
+-- Verificação dos Selects
 
     SELECT * from tipoDeJogador;
     
     SELECT * from sistema;
     
     SELECT * FROM usuario;
-    
+
+-- Selects utilizados em views
+
+    SELECT nome, email, senha FROM Usuario;
+
     SELECT COUNT(fkSistema) AS "Quantidade total", 
             sistema.nome AS "Sistema escolhido" 
     FROM usuario 
@@ -116,4 +142,49 @@ INSERT INTO usuario (nome, email, senha, fkSistema, fkTipoDeJogador) VALUES
         JOIN tipoDeJogador ON fktipoDeJogador = idtipoDeJogador 
     GROUP BY tipoDeJogador.nome;
 
-SHOW TABLES;
+-- Criação dos Views
+
+    CREATE VIEW VerificacaoLogin AS SELECT nome, email, senha FROM usuario;
+
+    CREATE VIEW QuantidadeSistemaGráfico AS SELECT COUNT(fkSistema) AS "Quantidade total", 
+            sistema.nome AS "Sistema escolhido" 
+        FROM usuario 
+            JOIN sistema ON fkSistema = idSistema 
+        GROUP BY sistema.nome; 
+
+    CREATE VIEW QuantidadeTipoGráfico AS SELECT COUNT(fkTipoDeJogador) AS "Quantidade total", 
+            tipoDeJogador.nome AS "Tipo de jogador escolhido" 
+        FROM usuario 
+            JOIN tipoDeJogador ON fktipoDeJogador = idtipoDeJogador 
+        GROUP BY tipoDeJogador.nome;
+
+-- Testando os views
+
+    SELECT * FROM VerificacaoLogin WHERE nome = 'Robert Ferreira';
+
+    SELECT * FROM QuantidadeSistemaGráfico;
+
+    SELECT * FROM QuantidadeTipoGráfico;
+
+-- Criando a procedure para Inserir Usuario
+
+CREATE PROCEDURE InserirUsuario (
+    IN nome VARCHAR(255),
+    IN email VARCHAR(255),
+    IN senha VARCHAR(255),
+    IN fkSistema INT,
+    IN fkTipoDeJogador INT
+)
+BEGIN
+    INSERT INTO usuario (nome, email, senha, fkSistema, fkTipoDeJogador)
+    VALUES (nome, email, senha, fkSistema, fkTipoDeJogador);
+END;
+
+-- Limpando a tabela usuario (Apenas o usuario administrador até o momento)
+TRUNCATE TABLE usuario;
+
+-- Testando o Procedure;
+
+CALL InserirUsuario("Robert Ferreira", "robert.souza@sptech.school", "TotalmenteSegura@123", "1", "3");
+
+-- Verificar sobre o call ou se usa o Insert msm pq se não seria como matar uma barata com escopeta
--- web-data-viz-main/src/models/usuarioModel.js
+++ web-data-viz-main/src/models/usuarioModel.js
@@ -7,7 +7,7 @@ function cadastrar(nome, email, senha, fkSistema, fkTipo) {
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucaoSql = `
-        INSERT INTO usuario (nome, email, senha, fkSistema, fkTipoDeJogador) VALUES ('${nome}', '${email}', '${senha}', '${fkSistema}', '${fkTipo}');
+        CALL InserirUsuario ('${nome}', '${email}', '${senha}', '${fkSistema}', '${fkTipo}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
@@ -17,7 +17,7 @@ function cadastrar(nome, email, senha, fkSistema, fkTipo) {
 function autenticar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucaoSql = `
-        SELECT idUsuario, nome, email FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+        SELECT * FROM VerificacaoLogin WHERE email = '${email}' AND senha = '${senha}';
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
